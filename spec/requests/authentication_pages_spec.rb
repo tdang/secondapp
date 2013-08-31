1a680411e6ccb8dd52e53cb59da302d4a2cd017e
Toan Dang <toan.s.dang@gmail.com>
2013-08-30 20:01:58 -0700

    finished railstutorial

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -50,6 +50,34 @@ describe "AuthenticationPages" do
     describe "for non-signed-in users" do
       let(:user) { FactoryGirl.create(:user) }
 
+      describe "in the Users controller" do
+
+        describe "visiting the user index" do
+          before { visit users_path }
+          it { should have_title('Sign in') }
+        end
+
+        describe "visiting the edit page" do
+          before { visit edit_user_path(user) }
+          it { should have_title('Sign in') }
+        end
+
+        describe "submitting to the update action" do
+          before { patch user_path(user) }
+          specify { expect(response).to redirect_to(signin_path) }
+        end
+
+        describe "visiting the following page" do
+          before { visit following_user_path(user)}
+          it { should have_title('Sign in')}
+        end
+
+        describe "visiting the followers page" do
+          before { visit followers_user_path(user)}
+          it { should have_title('Sign in')}
+        end
+      end
+
       describe "when attempting to visit a protected page" do
         before do
           visit edit_user_path(user)
@@ -87,28 +115,10 @@ describe "AuthenticationPages" do
           end
 
           describe "submitting to the destroy action" do
-            before { delete microposts_path(FactoryGirl.create(:micropost))}
-            specify{ expect(response).to redirect_to(signin_path)}
-          end
-        end
-
-        describe "in the Users controller" do
-
-          describe "visiting the user index" do
-            before { visit users_path }
-            it { should have_title('Sign in') }
-          end
-
-          describe "visiting the edit page" do
-            before { visit edit_user_path(user) }
-            it { should have_title('Sign in') }
-          end
-
-          describe "submitting to the update action" do
-            before { patch user_path(user) }
+            before { delete micropost_path(FactoryGirl.create(:micropost)) }
             specify { expect(response).to redirect_to(signin_path) }
           end
-        end
+        end 
       end
 
       describe "as wrong user" do
@@ -136,6 +146,18 @@ describe "AuthenticationPages" do
         describe "submitting a DELETE request to the Users#destroy action" do
           before { delete user_path(user) }
           specify { expect(response).to redirect_to(root_url) }
+        end
+      end
+
+      describe "in the Relationships controller" do
+        describe "submitting to the create action" do
+          before { post relationships_path }
+          specify { expect(response).to redirect_to(signin_path)}
+        end
+
+        describe "submitting to the destroy action" do
+          before { delete relationship_path(1) }
+          specify { expect(response).to redirect_to(signin_path)}
         end
       end
     end
